52dbe16c3f0b5a7a708c59c22341b0de8f70f9d4
Govind Rajewar <abhirajewar87@gmail.com>
2024-06-12 12:26:06 +0530

    added database config and initialzation

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-const express = require('express')
-const dotenv = require('dotenv').config()
+const express = require("express");
+const dotenv = require("dotenv").config();
+const mongoose = require("mongoose");
 
-const app = express()
+const app = express();
 
+app.get("/", (req, res) => {
+  res.json({
+    message: "Server is live",
+  });
+});
 
-app.get('/', (req, res) => {
-  res.send('Server is live')
-})
+app.get("/health", (req, res) => {
+  res.json({
+    message: "API is healthy",
+  });
+});
 
 app.listen(process.env.PORT, () => {
-  console.log(`App Running at http://localhost:${process.env.PORT}`)
-})
+  console.log(`App Running at http://localhost:${process.env.PORT}`);
+
+  mongoose
+    .connect(process.env.MongoDB_URL)
+    .then(() => console.log("Server Connected to MongoDB"))
+    .catch((err) => console.log(err));
+});
